3e31eaedb9c5153d1fc2626825976b26b620e163
P.A.M. van Dam (Pascal) <pascal@poortier.nl>
2022-04-16 15:14:48 +0200

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pascal71/go-lib
+
+go 1.17
---
+++ init.go
@@ -0,0 +1,14 @@
+/**
+ * Author: Pascal van Dam
+ * File: init.go
+ */
+
+package lib
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("[go-lib] initializing ...")
+}
